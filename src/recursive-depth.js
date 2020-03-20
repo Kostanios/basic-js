d1b05611323b621ecde305bc4be8b805e00ce61f
Kostanios <59231276+Kostanios@users.noreply.github.com>
2020-03-20 19:28:45 +0300

    init: "start recursive-depth task"

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,31 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    calculateDepth( arr ) {
+        
+        var deepDarkArray = [];
+        var deepDark = 0;
+
+        function searchArrays(arr){
+            for(var i = 0 ; i < arr.length ; i++){
+                if(Array.isArray(arr[i])){
+                    deepDark +=1;
+                    searchArrays(arr[i]);
+                }
+            }
+            deepDark += 1;
+            deepDarkArray.push(deepDark);
+            deepDark = 0;
+        }
+
+        
+
+        searchArrays(arr);
+
+        var maxDeep = deepDarkArray[0];
+
+        for(var i = 0; i < deepDarkArray.length; i++){
+            if(maxDeep < deepDarkArray[i]){maxDeep = deepDarkArray[i];}
+        }
+
+        return maxDeep;
     }
 };
