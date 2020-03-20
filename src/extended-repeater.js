35efa68e28d5f5b9e67e8296f3cb8daab3b162e4
Kostanios <59231276+Kostanios@users.noreply.github.com>
2020-03-20 19:22:27 +0300

    init: " start extended-repeater task"

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,32 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+if(options.repeatTimes === undefined){var repeat = 1}
+    else {var repeat = options.repeatTimes;}
+
+    if(options.separator === undefined){var separator = "+"}
+    else {var separator = String(options.separator);}
+
+    if(options.addition === undefined){var addition = ""}
+     else { var addition = String(options.addition)};
+
+    if(options.additionSeparator === undefined){var additionSeparator  = ""}
+        else{var additionSeparator = String(options.additionSeparator);} 
+
+    if (options.additionRepeatTimes === undefined){var timeaddition = 1} 
+        else{var timeaddition  =  options.additionRepeatTimes ;}
+
+    var StringStr = String(str)
+
+    var recal = ""
+    for(i = 0; i < repeat ; i++){
+        recal += StringStr;
+        for(var k = 0 ; k <timeaddition ; k++){
+            recal += addition;
+            if(k+1 == timeaddition ){break}
+            recal += additionSeparator;
+        }
+        if(i+1 == repeat){break;}
+        recal += separator;
+    }
+    return  recal;
 };
   
