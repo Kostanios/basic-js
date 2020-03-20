66adca0a283aa55c366058eea121e1a98eddc4dd
Kostanios <59231276+Kostanios@users.noreply.github.com>
2020-03-20 19:18:55 +0300

    init:" start count-cats task"

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,11 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  if(matrix.length < 1){return 0}
+  var catsearsnumber = 0;
+  for(var i = 0; i < matrix.length ; i ++ ){
+    for(var k = 0; k < matrix[0].length ; k ++){
+        if(matrix[i][k]=='^^'){catsearsnumber++;}
+        
+    }
+  }
+  return catsearsnumber;
 };
