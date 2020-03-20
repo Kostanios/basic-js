504a1a757ef0935c6347a529f6f124ac42a77db3
Kostanios <59231276+Kostanios@users.noreply.github.com>
2020-03-20 19:20:50 +0300

    init:" start dream-team task"

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,36 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+ if(!Array.isArray(members)){return false}
+  var nameArray = [];
+  for(var i = 0; i < members.length ; i++){
+    if(typeof (members[i]) =="string"){
+      fakeArray = members[i].split(" ");
+      for(var k = 0 ; k <fakeArray.length ; k++){
+        if(fakeArray[k] !=" " && fakeArray[k] !=""){
+          nameArray.push(fakeArray[k]);
+          break;
+        }
+      }
+    } 
+
+  }
+
+  members = nameArray;
+
+  console.log(members);
+  
+  var recalName = '';
+  for(var i = 0 ; i < members.length - 1; i++){
+    for(var k = 0; k < members.length - 1; k++){
+    if(members[k][0].toLowerCase().charCodeAt() > members[k+1][0].toLowerCase().charCodeAt()){
+      var fake = members[k];
+      members[k] = members[k+1];
+      members[k+1] = fake;
+    }
+   }
+  }
+  for(var i = 0; i < members.length ; i++){
+    recalName+=members[i][0].toUpperCase();
+  }
+  console.log(recalName);
+  return recalName;
 };
